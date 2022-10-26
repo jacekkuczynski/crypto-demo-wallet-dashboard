7e4414eb9900bb16f5e1845ae9774c73b0860c10
Jacek Kuczyński <idea.jacek@gmail.com>
2022-10-26 21:44:31 +0200

    tests wip

---
+++ src/components/LogoutBtn/LogoutBtn.test.js
@@ -0,0 +1,8 @@
+import { LogoutBtn } from "./LogoutBtn";
+import { render } from "@testing-library/react";
+
+describe("LogoutBtn", () => {
+  it("renders component", () => {
+    const {} = render(<LogoutBtn />);
+  });
+});
